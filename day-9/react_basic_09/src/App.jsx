514d453dde177271339e51814578192b9fb9fa5b
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-04-27 18:41:02 +0530

    day 9 react

--- day-9/react_basic_09/src/App.jsx
+++ day-9/react_basic_09/src/App.jsx
@@ -1,12 +1,14 @@
 import Reff from "./components/Reff"
 import Refff2 from "./components/Refff"
+import Todos from "./components/Todos"
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
       {/* <Reff /> */}
-      <Refff2 />
+      {/* <Refff2 /> */}
+      <Todos />
     </div>
   )
 }
