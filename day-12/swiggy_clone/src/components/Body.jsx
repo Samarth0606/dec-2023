3f68fc5f866d91fc039f0af2fdac9fba8f191407
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-05-03 18:54:34 +0530

    day 12 react-project-1 codes

--- day-12/swiggy_clone/src/components/Body.jsx
+++ day-12/swiggy_clone/src/components/Body.jsx
@@ -1,23 +1,10 @@
+import ApiCalling from "./ApiCalling";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = ()=>{
 
-    let restaurantDetails = [
-        {
-            name:"Pizza Hut", 
-            rating:"4.2", 
-            time:"20-25mins", 
-            cuisines:"Pizza, Desi",
-            place:"Punjabi Bagh",
-        },
-        {
-            name:"Burger House", 
-            rating:"4", 
-            time:"50-55mins", 
-            cuisines:"Beverages, Burger",
-            place:"Karol Bagh",
-        },
-    ]
+    let restaurantDetails = ApiCalling();
+     
     return(
         <div>
             <h1 className="font-bold text-xl m-10">Restaurants with online food delivery</h1>
@@ -25,13 +12,6 @@ const Body = ()=>{
 
             <div className="flex flex-wrap w-10/12 m-auto">
                 <RestaurantCard restArray={restaurantDetails} />
-                {/* <RestaurantCard name="Food of Indians" />
-                <RestaurantCard name="The Burger House" />
-                <RestaurantCard name="Famous Lassi" />
-                <RestaurantCard name="The Kohli's" />
-                <RestaurantCard name="KFC" />
-                <RestaurantCard name="Burger King" />
-                <RestaurantCard name="Domino's" /> */}
             </div>
         </div>
     )
--- day-12/swiggy_clone/src/components/RestaurantCard.jsx
+++ day-12/swiggy_clone/src/components/RestaurantCard.jsx
@@ -3,13 +3,13 @@ const RestaurantCard = ({restArray}) => {
   return (
     restArray.map((restDetails)=>{
         return(
-            <div className="m-8">
-                <img className="w-58 h-42 rounded-2xl" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/6513abee-f904-4a0e-9b49-90d1cc29551c_1003414.jpg" alt="" />
-                <h1 className="font-bold">{restDetails.name}</h1>
-                <span className="font-bold" >{restDetails.rating}</span>
-                <span className="mx-3">{restDetails.time}</span>
-                <p> {restDetails.cuisines}</p>
-                <p>{restDetails.place}</p>
+            <div className="m-8 w-58">
+                <img className="w-58 h-42 rounded-2xl" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restDetails.info.cloudinaryImageId}`} alt="" />
+                <h1 className="font-bold">{restDetails.info.name}</h1>
+                <span className="font-bold" >{restDetails.info.avgRating}</span>
+                <span className="mx-3">{restDetails.info.sla.slaString}</span>
+                <p> {restDetails.info.cuisines[0]}</p>
+                <p>{restDetails.info.areaName}</p>
             </div>
 
         )
