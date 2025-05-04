2950e2ed6217238bdf8184b56f54e0b1489dbd6b
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-05-04 18:13:59 +0530

    day 13 swiggy project react

---
+++ day-13/swiggy_clone/src/components/Offer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Header from './Header'
+// import Footer from './Footer'
+
+const Offer = () => {
+  return (
+    <div>
+        {/* <Header /> */}
+        <div>Offer</div>
+        {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default Offer
---
+++ day-13/swiggy_clone/src/main.jsx
@@ -0,0 +1,38 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Offer from './components/Offer.jsx'
+import Help from './components/Help.jsx'
+import Signin from './components/Signin.jsx'
+import Body from './components/Body.jsx'
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children:[
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/offers",
+        element: <Offer />
+      },
+      {
+        path: "/signin",
+        element: <Signin />
+      },
+      {
+        path: "/help",
+        element: <Help />
+      }
+    ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={appRouter}></RouterProvider>
+)
