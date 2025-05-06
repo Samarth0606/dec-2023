3237d21c58432234f067be87a9a7152273d0cb37
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-05-06 20:01:22 +0530

    day 14 half react

---
+++ day-14/swiggy_clone/src/App.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+// import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+const App = ()=>{
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
+
